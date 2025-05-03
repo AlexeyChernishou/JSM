f204918b56d2c9c13187aa305ee43fc96915702d
Alexey <207075785+AlexeyChernishou@users.noreply.github.com>
2025-05-03 17:14:36 +0300

    Added mod metadata

--- src/main/java/com/jsm/JSM.java
+++ src/main/java/com/jsm/JSM.java
@@ -2,7 +2,15 @@ package com.jsm;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = "jsm", name = "JSM", version = "0.1.0-beta.1")
+@Mod(modid = JSM.MOD_ID, name = JSM.MOD_NAME, version = JSM.MOD_VERSION, dependencies = JSM.MOD_DEPENDENCIES, useMetadata = JSM.USE_META, acceptedMinecraftVersions = JSM.MC_VERSION, updateJSON = JSM.UPDATE_URL)
 public class JSM {
+	
+	public static final String MOD_ID = "jsm";
+	public static final String MOD_NAME = "JSM";
+	public static final String MOD_VERSION = "0.1.0-beta.1";
+	public static final String MOD_DEPENDENCIES = "required-after:forge@[14.23.5.2860,)";
+	public static final boolean USE_META = true;
+	public static final String MC_VERSION = "[1.12.2]";
+	public static final String UPDATE_URL = "https://raw.githubusercontent.com/AlexeyChernishou/JSM/refs/heads/main/updates.json";
 
 }
