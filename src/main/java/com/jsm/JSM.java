24b1b26ead72fce4ff77bf3e2ef9ebd84326cb13
Alexey <207075785+AlexeyChernishou@users.noreply.github.com>
2025-05-04 10:34:03 +0300

    Added mod creative tab

--- src/main/java/com/jsm/JSM.java
+++ src/main/java/com/jsm/JSM.java
@@ -1,6 +1,10 @@
 package com.jsm;
 
+import com.jsm.init.JSMCreativeTabs;
+
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
 @Mod(modid = JSM.MOD_ID, name = JSM.MOD_NAME, version = JSM.MOD_VERSION, dependencies = JSM.MOD_DEPENDENCIES, useMetadata = JSM.USE_META, acceptedMinecraftVersions = JSM.MC_VERSION, updateJSON = JSM.UPDATE_URL)
 public class JSM {
@@ -12,5 +16,12 @@ public class JSM {
 	public static final boolean USE_META = true;
 	public static final String MC_VERSION = "[1.12.2]";
 	public static final String UPDATE_URL = "https://raw.githubusercontent.com/AlexeyChernishou/JSM/refs/heads/main/updates.json";
+	
+	@EventHandler
+	public static void postInit(FMLPostInitializationEvent event) {
+		
+		JSMCreativeTabs.init();
+		
+	}
 
 }
---
+++ src/main/java/com/jsm/init/JSMCreativeTabs.java
@@ -0,0 +1,30 @@
+package com.jsm.init;
+
+import com.jsm.JSM;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class JSMCreativeTabs {
+	
+	public static final CreativeTabs JSM_MAIN = new CreativeTabs(CreativeTabs.getNextID(), JSM.MOD_ID + "." + "main") {
+		
+		@SideOnly(Side.CLIENT)
+		public ItemStack createIcon() {
+			
+			return new ItemStack(Items.STONE_PICKAXE);
+			
+		}
+		
+	};
+	
+	public static void init() {
+		
+		
+		
+	}
+
+}
