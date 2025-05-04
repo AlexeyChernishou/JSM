06673c292044073be50fd7262a21d391791f9c4f
Alexey <207075785+AlexeyChernishou@users.noreply.github.com>
2025-05-04 14:05:41 +0300

    Added recipes for copper stuff

--- src/main/java/com/jsm/init/JSMRecipes.java
+++ src/main/java/com/jsm/init/JSMRecipes.java
@@ -1,8 +1,13 @@
 package com.jsm.init;
 
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
 public class JSMRecipes {
 	
 	public static void init() {
+		
+		GameRegistry.addSmelting(JSMBlocks.COPPER_ORE, new ItemStack(JSMItems.COPPER_INGOT), 0.7F);
 		
 	}
 
